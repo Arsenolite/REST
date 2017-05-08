383888ce7b076841891caa402e94455566f567f7
瞿瀚盛 <quhansheng@gmail.com>
2017-05-08 23:44:10 +0800

    SpringMVC demo

---
+++ REST/src/main/java/com/qhs/rest/bean/Student.java
@@ -0,0 +1,95 @@
+package com.qhs.rest.bean;
+
+public class Student {
+	private int id;
+	private String name;
+	private String qq;
+	private String major;
+	private String start_date;
+	private String school;
+	private String onlineclass;
+	private String onlineno;
+	private String diarylink;
+	private String aim;
+	private String recommender;
+	private String censor;
+	private String wherefrom;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getQq() {
+		return qq;
+	}
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(String start_date) {
+		this.start_date = start_date;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getOnlineclass() {
+		return onlineclass;
+	}
+	public void setOnlineclass(String onlineclass) {
+		this.onlineclass = onlineclass;
+	}
+	public String getOnlineno() {
+		return onlineno;
+	}
+	public void setOnlineno(String onlineno) {
+		this.onlineno = onlineno;
+	}
+	public String getDiarylink() {
+		return diarylink;
+	}
+	public void setDiarylink(String diarylink) {
+		this.diarylink = diarylink;
+	}
+	public String getAim() {
+		return aim;
+	}
+	public void setAim(String aim) {
+		this.aim = aim;
+	}
+	public String getRecommender() {
+		return recommender;
+	}
+	public void setRecommender(String recommender) {
+		this.recommender = recommender;
+	}
+	public String getCensor() {
+		return censor;
+	}
+	public void setCensor(String censor) {
+		this.censor = censor;
+	}
+	public String getWherefrom() {
+		return wherefrom;
+	}
+	public void setWherefrom(String wherefrom) {
+		this.wherefrom = wherefrom;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
---
+++ REST/src/main/java/com/qhs/rest/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.qhs.rest.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		ModelAndView mav = new ModelAndView("index.jsp");
+		mav.addObject("msg","HelloSpringMVC");
+		return mav;
+	}
+
+}
