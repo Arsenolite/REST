051d1db99cd2c6865e28c294625823af608aa5c5
瞿瀚盛 <quhansheng@gmail.com>
2017-05-22 22:22:56 +0800

    2017年5月22日22:22:47修改，完成分页查询

--- REST/src/main/java/com/qhs/rest/controller/StudentController.java
+++ REST/src/main/java/com/qhs/rest/controller/StudentController.java
@@ -1,15 +1,37 @@
 package com.qhs.rest.controller;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.json.JSONArray;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import com.qhs.rest.bean.Student;
+import com.qhs.rest.mapper.StudentMapper;
 @Controller
 public class StudentController {
-	@RequestMapping(value="/student", method=RequestMethod,POST)
-	//未完成：处理传参，编写返回用的页面以及Mybatis语句。
-	 public ModelAndView addStudent() throws Exception {
-	 ModelAndView mav = new ModelAndView("index.jsp");
-    mav.addObject("msg","success");
-    return mav;
+   @RequestMapping(value="/students",method=RequestMethod.GET)
+   @ResponseBody
+   //分页查询所有用户，加入默认页数参数为1
+   //2017年5月22日21:37:13 未完成根据专业查询用户
+   public ModelAndView getList(@RequestParam(value="pn",required=false,defaultValue="1") Integer pn) throws IOException{
+       //指定mapper
+       StudentMapper mapper = new SqlSessionFactoryBuilder()
+               .build(Resources.getResourceAsStream("mybatis-config.xml")).openSession()
+               .getMapper(StudentMapper.class);
+       List<Student> list = new ArrayList<Student>();
+       //不带参数，pn=0，参数为0,10
+       //带2，参数为10，20
+       list = mapper.list((pn-1)*10,pn*10);
+       ModelAndView mav = new ModelAndView("students");
+       JSONArray json = new JSONArray();
+       json.put(list);
+       mav.addObject("msg",json);
+       return mav;
+   }
 }
--- REST/src/main/java/com/qhs/rest/mapper/StudentMapper.java
+++ REST/src/main/java/com/qhs/rest/mapper/StudentMapper.java
@@ -1,9 +1,19 @@
 package com.qhs.rest.mapper;
-
 import java.util.List;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import com.qhs.rest.bean.Student;
-public interface StudentMapper {
-   @Select("select * from signup")
-   public List<Student> list();  
+public interface  StudentMapper {
+   @Select("select * from signup limit #{start},#{count}")
+   public List<Student> list(@Param("start") int start, @Param("count")int count);  
+   @Select("select * from signup where id= #{id}")
+   public Student get(int id);
+   @Select("select * from signup where id= #{name}")
+   public Student getall(String name);
+   @Insert("INSERT INTO signup VALUES "
+           + "(UNIX_TIMESTAMP(now())*1000,UNIX_TIMESTAMP(now())*1000,null,#{name},#{qq},#{major},#{start_date},"
+           + "#{school},#{onlineclass},#{onlineno},#{diarylink},"
+           + "#{aim},#{recommender},#{censor},#{wherefrom})")
+   public int add(Student student);
+   @Delete("delete * from signup where id= #{id}")
+   public boolean delete(int id);
 }
