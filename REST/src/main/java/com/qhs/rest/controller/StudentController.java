5ef67cb6bbd5daa6e4b11f13e30c2d3ca7d78559
瞿瀚盛 <quhansheng@gmail.com>
2017-05-23 11:47:06 +0800

    2017年5月23日11:45:08 添加

--- REST/src/main/java/com/qhs/rest/controller/StudentController.java
+++ REST/src/main/java/com/qhs/rest/controller/StudentController.java
@@ -1,4 +1,5 @@
 package com.qhs.rest.controller;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -13,25 +14,77 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import com.qhs.rest.bean.Student;
 import com.qhs.rest.mapper.StudentMapper;
+
 @Controller
 public class StudentController {
-   @RequestMapping(value="/students",method=RequestMethod.GET)
-   @ResponseBody
-   //分页查询所有用户，加入默认页数参数为1
-   //2017年5月22日21:37:13 未完成根据专业查询用户
-   public ModelAndView getList(@RequestParam(value="pn",required=false,defaultValue="1") Integer pn) throws IOException{
-       //指定mapper
-       StudentMapper mapper = new SqlSessionFactoryBuilder()
-               .build(Resources.getResourceAsStream("mybatis-config.xml")).openSession()
-               .getMapper(StudentMapper.class);
-       List<Student> list = new ArrayList<Student>();
-       //不带参数，pn=0，参数为0,10
-       //带2，参数为10，20
-       list = mapper.list((pn-1)*10,pn*10);
-       ModelAndView mav = new ModelAndView("students");
-       JSONArray json = new JSONArray();
-       json.put(list);
-       mav.addObject("msg",json);
-       return mav;
-   }
+	
+		// 分页查询所有用户，加入默认页数参数为1
+		// 2017年5月22日21:37:13 未完成根据专业查询用户
+		//2017年5月23日10:36:20完成根据专业查询，批量查询方法完成
+	@RequestMapping(value = "/students", method = RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView getStudentsByMajor(@RequestParam(value = "pn", required = false, defaultValue = "1") Integer pn,
+			@RequestParam(value = "major", required = false, defaultValue = "all") String major) throws IOException {
+		// 指定mapper
+		StudentMapper mapper = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"))
+				.openSession().getMapper(StudentMapper.class);
+		List<Student> list = new ArrayList<Student>();
+		// 不带参数，pn=0，参数为0,10
+		// 带2，参数为10，20
+		//由于major参数并不是数据库里实际存储的字段。。所以使用switch case块来匹配
+		switch(major){
+		case "all":
+			list = mapper.list((pn - 1) * 10, pn * 10);
+			break;
+		case "java":
+			list = mapper.listByMajor("JAVA工程师",(pn - 1) * 10, pn * 10);
+			break;
+		case "frontend":
+			list = mapper.listByMajor("前端工程师",(pn - 1) * 10, pn * 10);
+			break;
+		case "op":
+			list = mapper.listByMajor("运维工程师",(pn - 1) * 10, pn * 10);
+			break;
+		case "ios":
+			list = mapper.listByMajor("iOS工程师",(pn - 1) * 10, pn * 10);
+			break;
+		case "android":
+			list = mapper.listByMajor("Android工程师",(pn - 1) * 10, pn * 10);
+			break;
+		case "ui":
+			list = mapper.listByMajor("UI设计师",(pn - 1) * 10, pn * 10);
+			break;
+		case "pm":
+			list = mapper.listByMajor("产品经理",(pn - 1) * 10, pn * 10);
+			break;
+		}
+
+		// 传回list
+		ModelAndView mav = new ModelAndView("students");
+		JSONArray json = new JSONArray();
+		json.put(list);
+		mav.addObject("msg", json);
+		return mav;
+	}
+	//在@RequestMapping中添加produces来进行编码转化，无效？
+	@RequestMapping(value = "/student/{name}", method = RequestMethod.GET, produces = "text/html;charset=UTF-8")
+	@ResponseBody
+	public ModelAndView getStudentsByName(@PathVariable String name,@RequestParam(value = "pn", required = false, defaultValue = "1") Integer pn) throws IOException{
+		//一句话Mapper
+		//疑问：每个方法里都要写一个Mapper，看起来复用性比较低，后期是不是可以用Spring插进来。。
+		StudentMapper mapper = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"))
+				.openSession().getMapper(StudentMapper.class);
+		List<Student> list = new ArrayList<Student>();
+		//使用PathVariable注解将RequestMapping里#{name}传入方法体内，并且作为参数传入mapper，取回list
+		System.out.println(name);
+		list = mapper.listByName(name,(pn - 1) * 10, pn * 10);
+		//传回结果list
+		ModelAndView mav = new ModelAndView("student");
+		JSONArray json = new JSONArray();
+		json.put(list);
+		mav.addObject("msg", json);
+		return mav;
+	}
+	
+	
 }
--- REST/src/main/java/com/qhs/rest/mapper/StudentMapper.java
+++ REST/src/main/java/com/qhs/rest/mapper/StudentMapper.java
@@ -1,19 +1,55 @@
 package com.qhs.rest.mapper;
+
 import java.util.List;
 import org.apache.ibatis.annotations.*;
 import com.qhs.rest.bean.Student;
-public interface  StudentMapper {
-   @Select("select * from signup limit #{start},#{count}")
-   public List<Student> list(@Param("start") int start, @Param("count")int count);  
-   @Select("select * from signup where id= #{id}")
-   public Student get(int id);
-   @Select("select * from signup where id= #{name}")
-   public Student getall(String name);
-   @Insert("INSERT INTO signup VALUES "
-           + "(UNIX_TIMESTAMP(now())*1000,UNIX_TIMESTAMP(now())*1000,null,#{name},#{qq},#{major},#{start_date},"
-           + "#{school},#{onlineclass},#{onlineno},#{diarylink},"
-           + "#{aim},#{recommender},#{censor},#{wherefrom})")
-   public int add(Student student);
-   @Delete("delete * from signup where id= #{id}")
-   public boolean delete(int id);
+
+public interface StudentMapper {
+	// 分页查询
+	@Select("select * from signup limit #{start},#{count}")
+	public List<Student> list(@Param("start") int start, @Param("count") int count);
+
+	// 根据专业分页查询
+	@Select("select * from signup where major= #{major} limit #{start},#{count}")
+	public List<Student> listByMajor(@Param("major")String major, @Param("start") int start, @Param("count") int count);
+
+	// 根据姓名查询
+	@Select("select * from signup where name= #{name} limit #{start},#{count}")
+	public List<Student> listByName(@Param("name")String name, @Param("start") int start, @Param("count") int count);
+
+	// 根据ID精确查询
+	@Select("select * from signup where id= #{id}")
+	public Student get(int id);
+
+	// 添加
+	@Insert("INSERT INTO signup VALUES "
+			+ "(UNIX_TIMESTAMP(now())*1000,UNIX_TIMESTAMP(now())*1000,null,#{name},#{qq},#{major},#{start_date},"
+			+ "#{school},#{onlineclass},#{onlineno},#{diarylink}," + "#{aim},#{recommender},#{censor},#{wherefrom})")
+	public int add(Student student);
+
+	// 删除
+	@Delete("delete * from signup where id = #{id}")
+	public boolean delete(int id);
+
+	// 修改
+	@Update("<script> " + 
+				"update signup" +
+				"update_at = (UNIX_TIMESTAMP(now())*1000" +
+					"<set>" + 
+						"<if test=\"name != null\">name=#{name},</if>"+ 
+						"<if test=\"qq != null\">price=#{qq},</if>" +
+						"<if test=\"major != null\">price=#{major},</if>" +
+						"<if test=\"start_date != null\">price=#{start_date},</if>" +
+						"<if test=\"school != null\">price=#{school},</if>" +
+						"<if test=\"onlineclass != null\">price=#{onlineclass},</if>" +
+						"<if test=\"onlineno != null\">price=#{onlineno},</if>" +
+						"<if test=\"diarylink != null\">price=#{diarylink},</if>" +
+						"<if test=\"aim != null\">price=#{aim},</if>" +
+						"<if test=\"recommender != null\">price=#{recommender},</if>" +
+						"<if test=\"censor != null\">price=#{censor},</if>" +
+						"<if test=\"wherefrom != null\">price=#{wherefrom},</if>" +
+					"</set>" +
+				"where id = #{id}" + 
+			"</script>")
+	public boolean update(Student student);
 }
